0a184b41b5c262e7f504976642f2fcb794ac645e
Vergeo Valentino Gunawan <vergeovalentinog@gmail.com>
2023-05-12 21:04:11 +0700

    updating github profile web

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.querySelector(".about-btn").onclick = function() {
+        console.log("about");
+    }
+    document.querySelector(".achievement-btn").onclick = function() {
+        console.log("achievement");
+    }
+    document.querySelector(".contact-btn").onclick = function() {
+        console.log("contact");
+    }
+})
